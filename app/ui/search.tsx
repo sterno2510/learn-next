dd105c6005693441764a310015485e1693cfac4c
Brian Stern <brian.stern2511@gmail.com>
2024-10-08 15:17:31 -0400

    added debouncer

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -9,6 +9,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = (term: string) => {
+    console.log(`Searching... ${term}`);
     const params = new URLSearchParams(searchParams);
 
     if (term) {
