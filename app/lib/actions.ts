68f29656910fa22dc296a57c97a8d0b73900de10
Brian Stern <brian.stern2511@gmail.com>
2024-10-17 11:59:48 -0400

    added error uis

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -65,6 +65,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  throw new Error("Failed to Delete Invoice");
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {}
