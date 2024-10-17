fd7aaba5dc27100d7dab8eb0dd2f538d1bf253d2
Brian Stern <brian.stern2511@gmail.com>
2024-10-17 15:31:01 -0400

    added error messaging to creat form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -70,12 +70,21 @@ export async function createInvoice(prevState: State, formData: FormData) {
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get("customerId"),
     amount: formData.get("amount"),
     status: formData.get("status"),
   });
 
+  if (!validatedFields.success) {
+    console.log("NOT VALID");
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing Fields. Failed to Create Invoice.",
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
